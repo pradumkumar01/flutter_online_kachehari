81799986c244071d1dc7a535e7a9632d76e48315
ashutosh sharma <sashutosh186@gmail.com>
2024-09-12 13:33:01 +0530

    create a drawer in home page

--- lib/Screen/Home.dart
+++ lib/Screen/Home.dart
@@ -31,31 +31,80 @@ class _HomeState extends State<Home> {
                   Scaffold.of(context).openDrawer();
                 },
                 icon: Icon(Icons.menu,
-                color: Colors.white,));
+                  color: Colors.white,));
           },
         ),
         title: Text("Home"),
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Colors.deepPurpleAccent,
       ),
       drawer: Drawer(
+        backgroundColor: Colors.deepPurpleAccent,
         child: ListView(
+          shrinkWrap: true,
           padding: EdgeInsets.zero,
           children: [
-            DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Text("Drawer Header")),
+            Padding(
+              padding: const EdgeInsets.only(top: 40,bottom: 10),
+              child: CircleAvatar(backgroundImage: AssetImage("assets/images/login.png"),
+                radius: 80,
+              ),
+            ),
+            Center(child: Text('Name',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold,fontSize: 20),)),
+            SizedBox(height: 10,),
             ListTile(
-              title: Text("Home"),
+              leading: Icon(Icons.home,size: 50,color: Colors.white),
+              title: Text('Home',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),),
               onTap: () {},
             ),
             ListTile(
-              title: Text("Share"),
+              leading:Icon(Icons.account_circle_rounded,size: 50,color: Colors.white),
+              title: Text('Profile',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),),
               onTap: () {},
             ),
             ListTile(
-              title: Text("About"),
+              leading:Icon(Icons.settings,size: 50,color: Colors.white),
+              title: Text('Setting',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),),
+              onTap: () {},
+            ),
+            ListTile(
+              leading:Icon(Icons.message_outlined,size: 50,color: Colors.white),
+              title: Text('Blogs',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),),
+              onTap: () {},
+            ),
+            ListTile(
+              leading:Icon(Icons.support_outlined,size: 50,color: Colors.white),
+              title: Text('Support',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),),
+              onTap: () {},
+            ),
+            Divider(color: Colors.white,),
+            ListTile(
+              leading:Icon(Icons.file_copy_outlined,size: 50,color: Colors.white),
+              title: Text('Documents',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),),
+              onTap: () {},
+            ),
+            ListTile(
+              leading:Icon(Icons.policy,size: 50,color: Colors.white),
+              title: Text('Policy',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),),
+              onTap: () {},
+            ),
+            ListTile(
+              leading:Icon(Icons.miscellaneous_services,size: 50,color: Colors.white),
+              title: Text('Service',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),),
+              onTap: () {},
+            ),
+            ListTile(
+              leading:Icon(Icons.info_outline,size: 50,color: Colors.white),
+              title: Text('About',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),),
+              onTap: () {},
+            ),
+            ListTile(
+              leading:Icon(Icons.filter_none,size: 50,color: Colors.white),
+              title: Text('Terms & Condition',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),),
+              onTap: () {},
+            ),
+            ListTile(
+              leading:Icon(Icons.logout,size: 50,color: Colors.white),
+              title: Text('Logout',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),),
               onTap: () {},
             ),
           ],
@@ -64,9 +113,9 @@ class _HomeState extends State<Home> {
       body: Container(
         decoration: BoxDecoration(
             image: DecorationImage(
-          image: AssetImage("assets/images/background.jpg"),
-          fit: BoxFit.cover,
-        )),
+              image: AssetImage("assets/images/background.jpg"),
+              fit: BoxFit.cover,
+            )),
         child: ListView(
           children: [
             // Container(
@@ -89,3 +138,4 @@ class _HomeState extends State<Home> {
     );
   }
 }
+g
